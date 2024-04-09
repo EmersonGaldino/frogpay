260c2b53e29d86ef4fe1f17d24c500ba81b71344
EmersonGaldino <emersongaldino@hotmail.com>
2024-04-09 20:54:31 -0300

    chore: Contrato do repositorio de usuario

---
+++ frogpay.domain/Repositories/IRepository/User/IUserRepository.cs
@@ -0,0 +1,11 @@
+using System.Threading.Tasks;
+using frogpay.domain.Entity.User;
+using frogpay.domain.Repositories.IRepository.Base;
+
+namespace frogpay.domain.Repositories.IRepository.User;
+
+public interface IUserRepository : IBaseRepository<UserEntity>
+{
+    Task<UserEntity> GetUser(UserEntity model);
+    Task<UserEntity> GetAll();
+}
