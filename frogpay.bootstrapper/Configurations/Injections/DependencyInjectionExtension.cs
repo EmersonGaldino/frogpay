3c7ed696cf8f3121ea68097182a0f76dfe2e72d3
EmersonGaldino <emersongaldino@hotmail.com>
2024-04-09 22:07:17 -0300

    chore: Criando componente que injeta todas as classes de service e repositorios

---
+++ frogpay.bootstrapper/Configurations/Injections/DependencyInjectionExtension.cs
@@ -0,0 +1,45 @@
+using frogpay.application.AppService.User;
+using frogpay.application.Interface.User;
+using frogpay.bootstrapper.Configurations.Security;
+using frogpay.domain.Repositories.IRepository.User;
+using frogpay.domain.Service.User;
+using frogpay.repository.context;
+using frogpay.repository.User;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace frogpay.bootstrapper.Configurations.Injections;
+
+public static class DependencyInjectionExtension
+{
+    public static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        #region .::Include injection configurations token
+
+        var tokenConfig = new TokenConfig();
+        new ConfigureFromConfigurationOptions<TokenConfig>(configuration.GetSection("TokenConfig"))
+            .Configure(tokenConfig);
+        services.AddSingleton(tokenConfig);
+        #endregion
+        
+        #region .::AppServices
+        services.AddScoped<IUserAppService, UserAppService>();
+        #endregion
+        
+        #region .::Services
+        services.AddScoped<IUserService, UserService>();
+        #endregion
+        
+        #region .::Repositories
+        services.AddScoped<IUserRepository,UserRepository>();
+        #endregion
+        
+        #region .::UnitOfWork
+        
+        services.AddScoped<ContextDb>();
+        #endregion
+        
+        return services;
+    }
+}
