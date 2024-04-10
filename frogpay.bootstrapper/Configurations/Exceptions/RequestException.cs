d623dcfe33ae0d646c93505b9a8c1e12cdcef1d4
EmersonGaldino <emersongaldino@hotmail.com>
2024-04-09 22:11:17 -0300

    chore: Excecoes de requisicoes api

---
+++ frogpay.bootstrapper/Configurations/Exceptions/RequestException.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace frogpay.bootstrapper.Configurations.Exceptions;
+
+public class RequestException : Exception
+{
+    private string ErrorMessage { get; set; }
+    private int StatusCode { get; set; }
+    public RequestException(int statusCode, string message)
+    {
+        StatusCode = statusCode;
+        ErrorMessage = message;
+    }
+}
