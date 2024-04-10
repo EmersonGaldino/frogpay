f7394af3fe0f9482759bd855094d636efea4e3ad
EmersonGaldino <emersongaldino@hotmail.com>
2024-04-10 14:35:54 -0300

    feat: Criando crud de account, metodos ainda não implementados

---
+++ frogpay.domain/Repositories/IRepository/Account/IBankRepository.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using frogpay.domain.Entity.Bank;
+using frogpay.domain.Repositories.IRepository.Base;
+
+namespace frogpay.domain.Repositories.IRepository.Account;
+
+public interface IBankRepository : IBaseRepository<DataBankEntity>
+{
+    Task<DataBankEntity> GetAccount(DataBankEntity model);
+    Task<List<DataBankEntity>> GetAll();
+    Task<bool> CreateAccount(DataBankEntity model);
+    Task<DataBankEntity> UpdateAccount(DataBankEntity map);
+    Task<DataBankEntity> GetAccountByUserId(Guid userId);
+    Task<bool> DeleteAccount(Guid userId);
+}
---
+++ frogpay.repository/Account/BankRepository.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using frogpay.domain.Entity.Bank;
+using frogpay.domain.Repositories.IRepository.Account;
+using frogpay.repository.context;
+using Microsoft.EntityFrameworkCore;
+
+namespace frogpay.repository.Account;
+
+public class BankRepository : BaseRepository<DataBankEntity>, IBankRepository
+{
+    private DbContext context;
+    public BankRepository(ContextDb context) : base(context)
+    {
+        this.context = context;
+    }
+
+    public Task<DataBankEntity> GetAccount(DataBankEntity model)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<DataBankEntity>> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> CreateAccount(DataBankEntity model)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<DataBankEntity> UpdateAccount(DataBankEntity map)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<DataBankEntity> GetAccountByUserId(Guid userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> DeleteAccount(Guid userId)
+    {
+        throw new NotImplementedException();
+    }
+}
