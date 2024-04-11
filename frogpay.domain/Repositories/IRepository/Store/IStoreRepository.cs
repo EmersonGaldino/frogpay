705fd6c92330c39b28731ea8031e1e005182a951
EmersonGaldino <emersongaldino@hotmail.com>
2024-04-11 12:26:34 -0300

    feat: repositorio e interface criado, sem impletação

--- frogpay.domain/Repositories/IRepository/Store/IStoreRepository.cs
+++ frogpay.domain/Repositories/IRepository/Store/IStoreRepository.cs
@@ -1,6 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using frogpay.domain.Entity.Store;
+
 namespace frogpay.domain.Repositories.IRepository.Store;
 
 public interface IStoreRepository
 {
-    
+    Task<StoreEntity> GetAccount(StoreEntity model);
+    Task<List<StoreEntity>> GetAll();
+    Task<bool> CreateAccount(StoreEntity model);
+    Task<StoreEntity> UpdateAccount(StoreEntity map);
+    Task<StoreEntity> GetAccountByUserId(Guid userId);
+    Task<bool> DeleteAccount(Guid account_id);
 }
--- frogpay.repository/Store/StoreRepository.cs
+++ frogpay.repository/Store/StoreRepository.cs
@@ -1,3 +1,8 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using frogpay.domain.Entity.Store;
+using frogpay.domain.Repositories.IRepository.Store;
 using frogpay.repository.context;
 
 namespace frogpay.repository.Store;
@@ -8,5 +13,35 @@ public class StoreRepository : IStoreRepository
     public StoreRepository(ContextDb context)
     {
         this.context = context;
+    }
+
+    public Task<StoreEntity> GetAccount(StoreEntity model)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<StoreEntity>> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> CreateAccount(StoreEntity model)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<StoreEntity> UpdateAccount(StoreEntity map)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<StoreEntity> GetAccountByUserId(Guid userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> DeleteAccount(Guid account_id)
+    {
+        throw new NotImplementedException();
     }
 }
